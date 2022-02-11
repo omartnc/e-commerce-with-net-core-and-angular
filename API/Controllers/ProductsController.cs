870c5ecb277b13b2abb4b528bd5e0c7e4c1c9043
Ömer TUNÇ <omer_tunc_04@hotmail.com>
2022-02-10 23:00:46 -0500

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "GetProducts";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "GetProduct";
+        }
+    }
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace API.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
