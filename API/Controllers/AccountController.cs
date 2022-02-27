dd8a319192150eba8a4de38b1e7c00452661cf76
omartnc <omer_tunc_04@hotmail.com>
2022-02-27 18:15:21 -0500

    Api - validation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -79,6 +79,9 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistAsync(registerDto.Email).Result.Value)
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email Address is in use." } });
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,12 +1,22 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class AddressDto
     {
+        [Required]
+        public string Id { get; set; }
+        [Required]
         public string FistName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string ZipCode { get; set; }
     }
 }
